c4adcc04557b246507d0f644dc67057ca8043e95
keshan-dev <keshans.official@gmail.com>
2025-09-02 01:49:57 +0530

    add connection with database and change file name

---
+++ includes/database_connection.php
@@ -0,0 +1,14 @@
+<!-- database connection configure -->
+<?php
+$dsn = "mysql:host=;dbname=";
+$dbusername = "";
+$dbpassword = "";
+
+try {
+        $pdo = new PDO($dsn, $dbusername, $dbpassword);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+
+}
+?>
