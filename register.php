e9da6e2a07d2a7af08b8a5ccf7c1a726537a9bc0
VishwaChandeepa <chandeepavishwa@gmail.com>
2025-09-03 10:39:56 +0530

    Added user registration feature

--- register.php
+++ register.php
@@ -1,1 +1,23 @@
-<!-- register file  -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register and login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    
+</head>
+<body>
+    <div class="container" id="signup"></div>
+    <h1 class="form-title">Register</h1>
+    <form method="post" action="">
+        <div class="input-group">
+            <i class="fas fa-user"></i>
+            <input type="text" name="fname" id="fname" placeholder="First Name" required>
+
+        </div>
+    
+
+
+</body>
+</html>
